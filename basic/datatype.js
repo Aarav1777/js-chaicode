203f732153d9cf3e84674ba1d594267103247dd6
Aarav1777 <128198988+Aarav1777@users.noreply.github.com>
2024-08-30 08:25:29 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,17 @@
+"use strict";// treat all js code as a newer version 
+// alert(3+3) // we are use nodejs , not browser 
+console.log(3+3)
+console.log("hii")
+// number => 2 to power 53
+// bigint 
+//string 
+//boolean
+let isLoggedin = true;
+console.log(isLoggedin);
+
+// null
+// undefined
+// symbol
+//
+console.log(typeof null)// object 
+console.log(typeof undefined) // undefined 
